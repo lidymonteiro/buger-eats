fb7f942aa3abb890d90530d91f657d92f61121c9
Lidiane Monteiro <contato.lidymonteiro@hotmail.com>
2022-06-21 16:41:39 -0300

    feat: Faker with data and CPF they

--- cypress/factories/SignupFactory.js
+++ cypress/factories/SignupFactory.js
@@ -1,10 +1,16 @@
+var faker = require('faker')
+var cpf = require('gerador-validador-cpf')
+
 export default {
     deliver: function() {
 
+        var firstName = faker.name.firstName()
+        var lastName = faker.name.lastName()
+
         var data = {
-            name: 'Lidiane Monteiro',
-            cpf: '07798323876',
-            email: 'lidy@hotmail.com',
+            name: `${firstName} ${lastName}`,
+            cpf: cpf.generate(),
+            email: faker.internet.email(firstName),
             whatsapp: '11999999999',
             address: {
                 postalcode: '52280519',
