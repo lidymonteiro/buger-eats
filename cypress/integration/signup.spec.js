a8adc9007020d76a4c38c3c0259b6a11ec4ec63f
Lidiane Monteiro <contato.lidymonteiro@hotmail.com>
2022-06-22 17:39:51 -0300

    refactor: new its on the block

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,10 +1,9 @@
 import signup from '../pages/SignupPage'
 import signupFactory from '../factories/SignupFactory'
 
-describe('Signup', function() {
+describe('Signup', function () {
 
-
-    it('User should be deliver', function() {
+    it('User should be deliver', function () {
 
         var deliver = signupFactory.deliver()
 
@@ -13,10 +12,10 @@ describe('Signup', function() {
         signup.submit()
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
         signup.modalContentShouldBe(expectedMessage)
-  
+
     })
 
-    it('Invalid document', function() {
+    it('Invalid document', function () {
 
         var deliver = signupFactory.deliver()
         deliver.cpf = '077983238BB'
@@ -28,7 +27,7 @@ describe('Signup', function() {
         signup.alertMessageShouldBe(expectedMessage)
     })
 
-    it('Invalid email', function() {
+    it('Invalid email', function () {
 
         var deliver = signupFactory.deliver()
         deliver.email = 'user.com.br'
@@ -38,6 +37,30 @@ describe('Signup', function() {
         signup.submit()
         const expectedMessage = 'Oops! Email com formato inválido.'
         signup.alertMessageShouldBe(expectedMessage)
+    })
+
+    context('Required fields', function () {
+
+        const messages = [
+            { field: 'name', output: 'É necessário informar o nome' },
+            { field: 'cpf', output: 'É necessário informar o CPF' },
+            { field: 'email', output: 'É necessário informar o email' },
+            { field: 'postalcode', output: 'É necessário informar o CEP' },
+            { field: 'number', output: 'É necessário informar o número do endereço' },
+            { field: 'delivery', output: 'Selecione o método de entrega' },
+            { field: 'cnh', output: 'Adicione uma foto da sua CNH' }
+        ]
+
+        before(function(){
+            signup.go()
+            signup.submit()
+        })
+
+        messages.forEach(function(msg){
+            it(`${msg.field} is required`, function(){
+                signup.alertMessageShouldBe(msg.output)
+            })
+        })
     })
 
 })
